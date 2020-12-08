522dc5477ad2cd5d12e4ed760c7426bd04be13c0
Stjepan <turk.stipe@gmail.com>
2020-12-08 12:53:26 +0100

    Gyroscope, Accelerometer class added

--- app/src/main/java/com/example/lift/MainActivity.java
+++ app/src/main/java/com/example/lift/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.lift;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Accelerometer accelerometer;
+    private  Gyroscope gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.textView);
+
+        accelerometer = new Accelerometer(this);
+        gyroscope = new Gyroscope(this);
+
+        accelerometer.setListener(new Accelerometer.Listener() {
+            @Override
+            public void onTranslation(float tx, float ty, float tz) {
+               // text.setText("tx: " + Math.round(tx) + "ty: " + Math.round(ty) +"tz: "+ Math.round(tz));
+            }
+        });
+
+        gyroscope.setListener(new Gyroscope.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        accelerometer.register();
+        gyroscope.register();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        accelerometer.unregister();
+        gyroscope.unregister();
     }
 }
